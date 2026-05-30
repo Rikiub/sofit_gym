aa05e6c0c4033541fd0eef5532043ae84905a414
Rikiub <rikiub.dev@gmail.com>
2026-05-30 17:54:55 -0400

    Capacidad de setear la constante DEBUG a travez de env

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 // Constantes globales
 define('BASE_DIR', rtrim(dirname($_SERVER['SCRIPT_NAME']), '/\\'));
 define('ASSETS_DIR', BASE_DIR . '/assets');
-define('DEBUG', true);
+define('DEBUG', getenv("DEBUG") ?: true);
 
 // Cargar composer autoload y front controller
 require 'vendor/autoload.php';
